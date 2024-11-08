e5af3e0151c1a04e2fc801c19bd0ae2ff730e34b
Wagz28 <benwagstaff365@icloud.com>
2024-11-08 12:24:41 +0000

    script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
-const education = document.querySelector('.finance');
-const medicine = document.querySelector('.medicine');
-const finance = document.querySelector('.finance');
+const education = document.querySelector('.finance > h2');
+const medicine = document.querySelector('.medicine > h2');
+const finance = document.querySelector('.finance > h2');
 const HTTP = "http://127.0.0.1:5500/pages/"
 education.addEventListener("click", () => {
     location.href = HTTP + "education.html"

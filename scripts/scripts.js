9b26211403b79211d8f3982630289f90403a5083
Wagz28 <benwagstaff365@icloud.com>
2024-11-08 12:26:17 +0000

    last

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,1 @@
-const education = document.querySelector('.finance > h2');
-const medicine = document.querySelector('.medicine > h2');
-const finance = document.querySelector('.finance > h2');
-const HTTP = "http://127.0.0.1:5500/pages/"
-education.addEventListener("click", () => {
-    location.href = HTTP + "education.html"
-});
 
-finance.addEventListener("click", () => {
-    location.href = HTTP + "finance.html"
-});
-
-medicine.addEventListener("click", () => {
-    location.href = HTTP + "medicine.html"
-});

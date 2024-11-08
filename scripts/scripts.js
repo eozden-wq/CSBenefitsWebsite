95ea3744c73fec010b34ac05644486e0797c058b
Wagz28 <benwagstaff365@icloud.com>
2024-11-08 12:21:28 +0000

    click

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,14 +2,14 @@ const education = document.querySelector('.finance');
 const medicine = document.querySelector('.medicine');
 const finance = document.querySelector('.finance');
 const HTTP = "http://127.0.0.1:5500/pages/"
-education.addEventListener("contextmenu", () => {
+education.addEventListener("click", () => {
     location.href = HTTP + "education.html"
 });
 
-finance.addEventListener("contextmenu", () => {
+finance.addEventListener("click", () => {
     location.href = HTTP + "finance.html"
 });
 
-medicine.addEventListener("contextmenu", () => {
+medicine.addEventListener("click", () => {
     location.href = HTTP + "medicine.html"
 });
